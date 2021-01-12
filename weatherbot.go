52af9d50fd9dd42979a058bd7d67e93dce7f447a
Markiiiiiiii <bs255200@163.com>
2021-01-12 11:28:10 +0800

    2021-1-12 v1

---
+++ weatherbot.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"net/url"
+
+	simplejson "github.com/bitly/go-simplejson"
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
+)
+
+func main() {
+	proxyUrl, err := url.Parse("http://127.0.0.1:8001")                                  //设置代理http或sock5
+	myClient := &http.Client{Transport: &http.Transport{Proxy: http.ProxyURL(proxyUrl)}} //使用代理
+	bot, err := tgbotapi.NewBotAPIWithClient("1479970937:AAFKPdrFUmUCsnF4waRr4dZmLieY9Qj76-U", myClient)
+	if err != nil {
+		log.Panic(err)
+	}
+	bot.Debug = true
+	log.Printf("Authorized on account %s", bot.Self.UserName)
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
+	updates, err := bot.GetUpdatesChan(u)
+	for update := range updates {
+		if update.Message == nil {
+			// ignore any non-Message Updates
+			continue
+		}
+		weather := getinfo(update.Message.Text)
+		fmt.Println(weather)
+		msg := tgbotapi.NewMessage(update.Message.Chat.ID, weather)
+		msg.ReplyToMessageID = update.Message.MessageID
+		if _, err := bot.Send(msg); err != nil {
+			log.Panic(err)
+		}
+	}
+}
+
+// 获取城市天气
+func getinfo(cityname string) string {
+	URL := "https://way.jd.com/he/freeweather?appkey=79d6586692c7cb8127acf38815f8f6c8&city=" + cityname
+	queryUrl := fmt.Sprintf("%s", URL)
+	resp, err := http.Get(queryUrl)
+	if err != nil {
+		log.Println(err)
+	}
+
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+	res, err := simplejson.NewJson(body)
+	qlty, _ := res.Get("result").Get("HeWeather5").GetIndex(0).Get("aqi").Get("city").Get("qlty").String()
+	return qlty
+}
