7db22cb6876f1f0a84ee16de2297492288e53b1d
Markiiiiiiii <bs255200@163.com>
2021-03-21 15:06:28 +0800

    fixed

--- bot/server.go
+++ bot/server.go
@@ -2,52 +2,47 @@ package bot
 
 import (
 	"fmt"
-	"io/ioutil"
-	"net/http"
 	"time"
 
-	"log"
-
 	simplejson "github.com/bitly/go-simplejson"
+	"go.uber.org/zap"
 	tb "gopkg.in/tucnak/telebot.v2"
+	"gotgbot.com/v0.01/config"
+	"gotgbot.com/v0.01/fetch"
 )
 
 //按照传入的参数搜索
 func searchByText(t *tb.Message) {
+	zap.S().Infow("Handle func searchByText")
 	if t.Text != "" {
 		keyWord := t.Payload
-		dates, _ := getAPIdate(keyWord, 1)
+		dates, _ := getAPIdate(keyWord, config.Page)
 		var strs string
 		for _, str := range dates {
 			strs += str + "\n"
 		}
 		B.Send(t.Chat, strs)
-
+		time.Sleep(time.Second * 10)
 	}
 
 }
 
-func getAPIdate(keyword string, pageNumber int) (date []string, err error) {
-	if pageNumber <= 0 {
-		pageNumber = 1
-	}
+func getAPIdate(keyWord string, pageNumber int) (date []string, err error) {
+	zap.S().Infow("Keyword", keyWord,
+		"Pagenumber", pageNumber)
 	utmp := "https://api.dalipan.com/api/v1/pan/search?t=%d&kw=%s&page=%d&line=1&site=dalipan"
-	url := fmt.Sprintf(utmp, time.Now().Unix(), keyword, pageNumber)
-	fmt.Println(url)
+	url := fmt.Sprintf(utmp, time.Now().Unix(), keyWord, pageNumber)
 
-	resp, err := http.Get(url)
-
+	body, err := fetch.Fetch(url)
 	if err != nil {
-		log.Printf("Don't get the url,error:%s\n", err)
-		return nil, err
-	}
-	defer resp.Body.Close()
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		log.Printf("Read response date error ,error:%s\n", err)
+		zap.S().Errorw("Read response date error ", err)
 		return nil, err
 	}
 	jsondate, err := simplejson.NewJson(body)
+	if err != nil {
+		zap.S().Errorw("SimpleJSON Umshall error", err)
+		return nil, err
+	}
 	date = []string{}
 	for i := 0; i < 29; i++ {
 		tmpdate, _ := jsondate.Get("resources").GetIndex(i).Get("res").Get("filename").String()
---
+++ fetch/fetch.go
@@ -0,0 +1,36 @@
+package fetch
+
+import (
+	"io/ioutil"
+	"net/http"
+
+	"go.uber.org/zap"
+)
+
+func Fetch(url string) ([]byte, error) {
+	client := &http.Client{}
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		zap.S().Errorw("Create a new request error", err)
+		return nil, err
+	}
+	//构建访问头
+	req.Header.Add("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9")
+	req.Header.Add("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 11_2_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Safari/537.36")
+	req.Header.Add("Host", "api.dalipan.com")
+	req.Header.Add("Accept-Language", "zh-CN,zh;q=0.9")
+	req.Header.Add("Cache-Control", "max-age=0")
+
+	//访问
+	reps, err := client.Do(req)
+	if err != nil {
+		zap.S().Errorw("Don't get a response error", err)
+		return nil, err
+	}
+	defer reps.Body.Close()
+
+	if reps.StatusCode != http.StatusOK {
+		zap.S().Infow("Don't get date from the URL")
+	}
+	return ioutil.ReadAll(reps.Body)
+}
