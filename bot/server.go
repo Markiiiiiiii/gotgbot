b9dace08948b9598e3343e815849f42f482fcae9
Markiiiiiiii <bs255200@163.com>
2021-03-23 16:32:05 +0800

    error in searver.go

--- bot/server.go
+++ bot/server.go
@@ -1,12 +1,12 @@
 package bot
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/url"
 	"reflect"
 	"time"
 
-	simplejson "github.com/bitly/go-simplejson"
 	"go.uber.org/zap"
 	tb "gopkg.in/tucnak/telebot.v2"
 	"gotgbot.com/v0.01/config"
@@ -14,16 +14,18 @@ import (
 )
 
 //定义返回数据类型
-type NetDiskDate struct {
-	title string
-	size  int64
-}
+// type NetDiskDate struct {
+// 	Title string
+// 	Size  int64
+// }
+// type ResultDate struct{
+// 	NetDiskDates []NetDiskDate
+// }
 
 //按照传入的参数搜索
 func searchByText(t *tb.Message) {
 	if t.Text != "" {
-		keyWord := t.Payload
-		dates, _ := getApiDate(keyWord, config.Page)
+		dates, _ := getApiDates(t.Payload,config.Page)
 		//判断返回值是否有被屏蔽的值
 		if _, ok := dates.(string); ok {
 			B.Send(t.Chat, dates)
@@ -34,7 +36,7 @@ func searchByText(t *tb.Message) {
 			strs := ""
 			url := "http://www.dalipan.com"
 			for j := 0; j < dateList.Len(); j++ {
-					fmt.Println(dateList.FieldByName("title"))
+					fmt.Println(dateList.FieldByName("Title"))
 
 					// field := dateList.Field(i)
 					// strs = strs + fmt.Sprintf("[%s](%s)-[%d]\n", field.Value, url)
@@ -54,34 +56,55 @@ func searchByText(t *tb.Message) {
 // func sendToTelgram (t *tb.Message,date interface{}){
 
 // }
-//调用API获取JSON内容
-func getApiDate(keyWord string, pageNumber int)(interface{},error) {
+//调用API
+func getApiDate(keyWord string,pageNumber int)(ResultDate,error){
+	var dates []NetDiskDate
 	zap.S().Infof("Search by Keyword: %s , Pagenumber: %d ", keyWord, pageNumber)
-	utmp := "https://api.dalipan.com/api/v1/pan/search?t=%d&kw=%s&page=%d&line=1&site=dalipan"
-	url := fmt.Sprintf(utmp, time.Now().Unix(), url.QueryEscape(keyWord), pageNumber)
+	url := fmt.Sprintf("https://api.dalipan.com/api/v1/pan/search?t=%d&kw=%s&page=%d&line=1&site=dalipan", time.Now().Unix(), url.QueryEscape(keyWord), pageNumber)
 	//请求数据
 	body, err := fetch.Fetch(url)
 	if err != nil {
 		zap.S().Errorf("Read response date error: %s ", err)
-		return nil, err
+		return nil,err
 	}
-	var dates []NetDiskDate
 	// 如果请求返回的数据一个私有类型则存在关键词被屏蔽返回信息
 	if string(body[:]) == "privacy" {
-		date := "关键词: <" + keyWord + "> 被屏蔽了"
-		return date, nil
+		return  nil,err
 	} else {
 		//序列化json数据
 		jsondate, err := simplejson.NewJson(body)
 		if err != nil {
 			zap.S().Errorf("SimpleJSON Unmarshal error: %s", err)
-			return nil, err
+			return nil,err
 		}
 
 		for i := 0; i < 29; i++ {
-			dates[i].title, _ = jsondate.Get("resources").GetIndex(i).Get("res").Get("filename").String()
-			dates[i].size, _ = jsondate.Get("resources").GetIndex(i).Get("res").Get("size").Int64()
+			&dates[i].Title, _ = jsondate.Get("resources").GetIndex(i).Get("res").Get("filename").String()
+			&dates[i].Size, _ = jsondate.Get("resources").GetIndex(i).Get("res").Get("size").Int64()
+		}
+		return dates,nil
+	}
+}
+
+func getApiDates(k string,num int)(baiduNetDiskDate,error){
+	var dates baiduNetDiskDate
+	zap.S().Infof("Search by Keyword: %s , Pagenumber: %d ", k, num)
+	url := fmt.Sprintf("https://api.dalipan.com/api/v1/pan/search?t=%d&kw=%s&page=%d&line=1&site=dalipan", time.Now().Unix(), url.QueryEscape(k), num)
+	//请求数据
+	body, err := fetch.Fetch(url)
+	if err != nil {
+		zap.S().Errorf("Read response date error: %s ", err)
+		return nil,err
+	}
+	if string(body[:]) == "privacy" {
+		return  nil,err
+	} else {
+				//序列化json数据
+		err :=json.Unmarshal(body,&dates)
+		if err!=nil {
+			zap.S().Errorf("Unmarshal date error: %s ", err)
+			return nil ,nil 
 		}
 	}
-	return dates, nil
+return dates,nil
 }
